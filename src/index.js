fa2126443146b47c1223255d68c514bbe64266c1
[Moran98] <[moranzhang@yeah.net]>
2022-08-03 10:51:43 +0800

    todolist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let nextListId = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default function Todolist() {
+  const [item, setItem] = useState('');
+  const [list, setList] = useState([]);
+  const [finishedlist, setFinishedlist] = useState([]);
+
+  console.log(nextListId);
+
+  return (
+    <>
+      <div>
+        <h1>TODOLIST</h1>
+        <input
+          value={item}
+          onChange={e => setItem(e.target.value)}
+        ></input>
+        <button onClick={() => {
+          setItem('');
+          //这里的作用方法，注释掉之后点击按钮不会渲染？
+          // let nextListId = 0;
+          setList(
+            [...list,
+            {
+              id: nextListId,
+              item: item,
+            }]);
+          nextListId++;
+        }}>add</button>
+        <hr />
+        <div>未完成</div>
+        <ul>
+          {list.map(todoItem => (
+            <>
+              <li key={todoItem.id}>
+                {todoItem.item}{'-'}
+                <button
+                  onClick={() => {
+                    setList(list.filter(a => a.id !== todoItem.id))
+                    setFinishedlist(finishedlist.concat(list.filter(a => a.id == todoItem.id)))
+                  }}
+                >done</button>
+              </li>
+            </>
+          ))}
+        </ul>
+        <hr />
+        <div>已完成</div>
+        <ul>
+          {finishedlist.map(finishedItem => (
+            <>
+              {/* <button onClick={() => {console.log(finishedlist)}}></button> */}
+              <li key = {finishedItem.id}>
+                {finishedItem.item}{'-'}
+              </li>
+            </>
+          ))
+          }
+        </ul>
+
+
+
+
+      </div>
+    </>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Todolist />);
